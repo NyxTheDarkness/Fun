cd1de7ea022c9fcd235a4319fbcbaf215567e525
Nyx <nyx@mindscape.world>
2016-06-13 14:37:21 +0800

    Attempt at a player

---
+++ Assets/Move.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+public class Move : MonoBehaviour {
+	public float speed = 30;
+
+	void FixedUpdate ()
+	{
+		float v = Input.GetAxisRaw("Horizontal");
+		GetComponent<Rigidbody2D>().velocity = new Vector2(0, v) * speed;
+	}
+}
