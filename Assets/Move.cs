ad3e496381adf8d999cb1790623a8c80ba2daaf1
Nyx <Nyx@mindscape.world>
2016-06-25 16:55:46 +0800

    minor edit

--- Assets/Move.cs
+++ Assets/Move.cs
@@ -2,10 +2,15 @@
 using System.Collections;
 public class Move : MonoBehaviour {
 	public float speed = 30;
+	float v;
+
+	void Update()
+	{
+		v = Input.GetAxisRaw("Horizontal");
+	}
 
 	void FixedUpdate ()
 	{
-		float v = Input.GetAxisRaw("Horizontal");
 		GetComponent<Rigidbody2D>().velocity += new Vector2(v, 0) * speed;
 	}
 }
